9ec480a44a9b9d75fc5b6990a5999e0d0fcdf853
Raj Kushwaha <rajkushwahacs105@gmail.com>
2026-05-30 23:17:34 +0530

    URL

--- src/app.js
+++ src/app.js
@@ -6,10 +6,15 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://frontend-ai-resume.vercel.app"
+    ],
+    credentials: true,
+  })
+);
 
 
 const authRouter = require("./routes/auth.route")
