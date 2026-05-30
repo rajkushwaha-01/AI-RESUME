3840318898d496a92b16091c1bb8a7cdde3ac967
Raj Kushwaha <rajkushwahacs105@gmail.com>
2026-05-30 23:37:42 +0530

    model

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -166,7 +166,7 @@ async function generateResumePdf({ resume, selfDescription, jobDescription }) {
                     `
 
     const response = await ai.models.generateContent({
-        model: "gemini-3-flash-preview",
+        model: "gemini-2.5-flash",
         contents: prompt,
         config: {
             responseMimeType: "application/json",
