59ac002882ca4fe74b0c6f90f465e6f0ed938ea8
Raj Kushwaha <rajkushwahacs105@gmail.com>
2026-05-31 07:59:43 +0530

    ...

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -199,8 +199,15 @@ async function generateResumePdf({ resume, selfDescription, jobDescription }) {
 
 
 async function generatePdfFromHtml(htmlContent) {
-
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage",
+            "--disable-gpu"
+        ]
+    });
 
     const page = await browser.newPage();
 
